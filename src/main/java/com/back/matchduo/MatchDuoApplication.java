b795c60b52b2343a01e76115c5f23758267f40ff
Jong woo <bjw709@gmail.com>
2025-12-16 22:09:16 +0900

    feat(auth): Auth 도메인 형태로 구성, 설정 (#30)

--- src/main/java/com/back/matchduo/MatchDuoApplication.java
+++ src/main/java/com/back/matchduo/MatchDuoApplication.java
@@ -2,8 +2,10 @@ package com.back.matchduo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
 
 @SpringBootApplication
+@ConfigurationPropertiesScan("com.back.matchduo")
 public class MatchDuoApplication {
 
     public static void main(String[] args) {
