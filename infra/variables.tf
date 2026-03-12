efa1e4f04a7d08207f588b04cd3a5cdd07e1fe24
HongRae-Kim <ghdfo7882@gmail.com>
2026-03-12 18:00:29 +0900

    chore(deploy): load env file and declare infra secrets

--- infra/variables.tf
+++ infra/variables.tf
@@ -7,3 +7,26 @@ variable "region" {
   description = "region"
   default     = "ap-northeast-2"
 }
+
+variable "mysql_password" {
+  description = "MySQL root and application user password"
+  type        = string
+  sensitive   = true
+}
+
+variable "redis_password" {
+  description = "Redis password"
+  type        = string
+  sensitive   = true
+}
+
+variable "github_access_token_1" {
+  description = "GitHub Container Registry access token for EC2 image pulls"
+  type        = string
+  sensitive   = true
+}
+
+variable "github_access_token_1_owner" {
+  description = "GitHub owner used for GHCR login on EC2"
+  type        = string
+}
